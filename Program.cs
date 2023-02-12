1c5b12b3c266bd19a2107a1a6d18412c115fefbd
Yaroslaw-aw <manumission@mail.ru>
2023-02-12 17:37:02 +0300

    13_task_final

--- Program.cs
+++ Program.cs
@@ -1,15 +1,11 @@
 ﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает,
 // что третьей цифры нет.
 
-Console.WriteLine("Введите трёхзначное число");
+Console.WriteLine("Введите число");
 
 string string_number = Console.ReadLine();
 
 if (string_number.Length >= 3)
-{
     Console.WriteLine(string_number[2]);
-}
 else
-{
     Console.WriteLine("третьей цифры нет");
-}
