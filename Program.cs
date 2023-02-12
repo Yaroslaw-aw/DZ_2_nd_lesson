c713d0b824986275fa98eaf49435fa73f647f335
Yaroslaw-aw <manumission@mail.ru>
2023-02-12 17:51:10 +0300

    Tasl_15_ready

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите цифру от 1 до 7");
+
+string day = Console.ReadLine();
+
+int day_number;
+int.TryParse(day, out day_number);
+
+if (5 < day_number && day_number <= 7) //if (day_number == 6 || day_nuber == 7)
+    Console.WriteLine($"{day} -> да");
+else
+    Console.WriteLine("нет");
+
